83fac6c8de526c8b62445d17ea5285073fabbc25
Víctor Cosme <victorcosmema@gmail.com>
2023-07-03 00:00:28 -0600

    Add third practice, third JS class

--- 1-front/3-javascript/practice/practice-1.3/main3.js
+++ 1-front/3-javascript/practice/practice-1.3/main3.js
@@ -1,2 +1,12 @@
 // Crar una función que reciba el nombre y los apellidos de una persona y devuelva
 // la cantidad de caracteres del nombre completo
+
+function fullName(name, surname){
+    let lengthFullName = (parseInt(name.lenght) + parseInt(surname.length));
+    console.log(typeof(name.length))
+    console.log(typeof(surname.length))
+    return typeof(lengthFullName);
+}
+
+console.log(fullName("Víctor", "Cosme"));
+console.log(typeof(fullName("Víctor", "Cosme")));
