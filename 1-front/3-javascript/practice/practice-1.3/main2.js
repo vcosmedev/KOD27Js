9cdeca971a42312b2a39df7991284c7a67eaf112
Víctor Cosme <victorcosmema@gmail.com>
2023-07-02 23:37:57 -0600

    Add second practice, third JS class

--- 1-front/3-javascript/practice/practice-1.3/main2.js
+++ 1-front/3-javascript/practice/practice-1.3/main2.js
@@ -1,3 +1,16 @@
 // Crear una función que reciba dos números y devuelva un texto con base a las siguientes condiciones:
 // - Si el resultado es par, devolver "El resultado es par"
 // - Si el resultado es impar, devolver "El resultado es impar"
+
+function getNumber(number){
+    if (number % 2 === 0) {
+        console.log("Result is an even number")
+    } else {
+        console.log("Result is an odd number")
+    }
+    return number;
+}
+
+getNumber(4);
+getNumber(7);
+getNumber(10);
