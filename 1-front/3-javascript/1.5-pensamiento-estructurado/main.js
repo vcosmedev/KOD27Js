480dc82203fefe759c8f9491cbf7d320b9e68fd2
Víctor Cosme <victorcosmema@gmail.com>
2023-07-08 22:44:14 -0600

    Add 'Pensamiento estructurado', 5th class

--- 1-front/3-javascript/1.5-pensamiento-estructurado/main.js
+++ 1-front/3-javascript/1.5-pensamiento-estructurado/main.js
@@ -1,1 +1,8 @@
 // Pensamiento estructurado
+
+// 1- Definición del problema
+// 2- Análisis del problema
+// 3- Definición de herramientas
+// 4- Análisis de herramientas
+// 5- Definición de soluciones
+// 6- Definición del problema
