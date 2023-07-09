72fd587b49b023f590c68cc0c842344ba9122208
Víctor Cosme <victorcosmema@gmail.com>
2023-07-09 00:37:44 -0600

    Add 5th exercise, 6th JS class

--- 1-front/3-javascript/1.6-arrays-methods-one/exercises/main5.js
+++ 1-front/3-javascript/1.6-arrays-methods-one/exercises/main5.js
@@ -1,2 +1,13 @@
-// Se requiere obtener un rarray que contenga los mismos nombres de cada país,
+// Se requiere obtener un array que contenga los mismos nombres de cada país,
 // pero escritos en mayúsculas
+
+const countries = ["México", "Canadá", "Brasil", "España"];
+
+let turnToUpperCase = (countries) => {
+    let upperCase = [];
+    for (let i = 0; i < countries.length; i++) {
+        upperCase[i] = countries[i].toUpperCase();
+    }
+    return upperCase;
+}
+console.log(turnToUpperCase(countries));
