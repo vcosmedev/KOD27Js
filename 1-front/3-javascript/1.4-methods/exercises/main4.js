ed8f3d41bdd89ae82bb3279a13b2d08a829143bf
Víctor Cosme <victorcosmema@gmail.com>
2023-07-08 22:16:59 -0600

    Add 4th exercise, 4th class

--- 1-front/3-javascript/1.4-methods/exercises/main4.js
+++ 1-front/3-javascript/1.4-methods/exercises/main4.js
@@ -2,3 +2,10 @@
 // con otra palabra. Por ejemplo:
 // "Mi perro es gris y es un latoso" | "es" | "no es"
 // Result -> "Mi perro no es gris y no es un latoso"
+
+const replaceWords = (string) => {
+    console.log(string.replace(/es/g, "no es"));
+}
+
+replaceWords("Mi perro es gris y es un latoso");
+replaceWords("Mi gato pequeño es marrón y es esquivo");
