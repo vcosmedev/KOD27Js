fc7b6110e886ebd00690214ab90ea951b858ff03
Víctor Cosme <victorcosmema@gmail.com>
2023-07-18 19:22:18 -0600

    Add exercises 14th session

---
+++ 1-front/3-javascript/1.14-dom-selection/exercises/js/main.js
@@ -0,0 +1,72 @@
+// 1- Imprimir nombre cmpleto de los Koders en una lista
+
+// 2- Impresa la lista, todos los Koders en posición par que tengan un color y 
+// todos los Koders en posición impar que tengan otro color
+
+// 3- Crear una nueva lista que contenga el nombre completo del Koder y su promedio:
+//    * Si el promedio es < 9.5 -> Texto que representa el promedio sea AMARILLO
+//    * Si el promedio es <= 9.5 -> Texto que representa el promedio sea VERDE
+
+const koders = [
+    {
+      name: "Jonhatan",
+      lastname: "Sanabria",
+      scores: {
+        html: 10,
+        css: 9,
+        js: 7,
+      },
+      generation: 27,
+    },
+    {
+      name: "Naomi",
+      lastname: "Lopez",
+      scores: {
+        html: 10,
+        css: 10,
+        js: 10,
+      },
+      generation: 9,
+    },
+    {
+      name: "Rurick",
+      lastname: "Maqueo",
+      scores: {
+        html: 8,
+        css: 9,
+        js: 9,
+      },
+      generation: 4,
+    },
+    {
+      name: "Brenda",
+      lastname: "González",
+      scores: {
+        html: 8,
+        css: 9,
+        js: 10,
+      },
+      generation: 4,
+    },
+    {
+      name: "Verónica",
+      lastname: "Corona",
+      scores: {
+        html: 8,
+        css: 8,
+        js: 10,
+      },
+      generation: 10,
+    },
+    {
+      name: "Karen",
+      lastname: "Corona",
+      scores: {
+        html: 9,
+        css: 9,
+        js: 9,
+      },
+      generation: 10,
+    },
+  ];
+  
