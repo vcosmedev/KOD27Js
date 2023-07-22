347abffc8b6ad82a3154c1774b29e1622d1384a2
Víctor Cosme <victorcosmema@gmail.com>
2023-07-21 19:02:57 -0600

    Add fetch exercise, 17th JS session

--- 1-front/3-javascript/1.17-asynchrony/exercise/js/main.js
+++ 1-front/3-javascript/1.17-asynchrony/exercise/js/main.js
@@ -30,12 +30,100 @@ const createComment = async (newComment) => {
   };
   
   document.getElementById("save-comment").addEventListener("click", () => {
-    comments = [...comments, { ...commentObject, id: new Date().getTime() }];
-    console.log(comments);
-    createComment(comments);
-    resetForm("comment-form");
-    commentObject = {};
+    comment = { ...commentObject, id: new Date().getTime() };
+    createComment(comment)
+        .then(()=> {
+            resetForm("comment-form");
+            commentObject = {};
+            printAllComments();
+        })
+        .catch((reason) => console.log("Message:" + reason.message));
   });
+
+  const deleteComment = async (id) => {
+    console.log(id)
+    let response = await fetch(
+      `https://javascript27g-default-rtdb.firebaseio.com/comments/${id}`,
+      {
+        method: "DELETE"
+      }
+    );
+    return await response.json();
+  };
+
+
+// Print comments
+  const createCommentBox = (commentData) => {
+    let { author, comment, id } = commentData;
+    let card = document.createElement("div");
+    card.classList.add("card");
+  
+    let cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+  
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("btn", "btn-danger");
+    deleteButton.innerText = "Borrar comentario";
+  
+    deleteButton.addEventListener("click", () => {
+      let comment = comments.filter((comment) => comment.id !== id);
+      deleteComment(comment)
+        .then(() => {
+            printAllComments();
+        })
+        .catch((reason) => console.log("Message:" + reason.message));
+    });
+  
+    let cardText = document.createTextNode(`${author} : ${comment}`);
+  
+    cardBody.append(cardText, deleteButton);
+    card.append(cardBody);
+  
+    return card;
+  };
+
+  const getAllComments = async () => {
+    let response = await fetch(
+      "https://javascript27g-default-rtdb.firebaseio.com/comments/.json",
+      {
+        method: "GET"
+      }
+    );
+    let data = await response.json();
+    return data;
+  };
+  
+  const printAllComments = () => {
+    let wrapper = document.getElementById("comment-wrapper");
+    wrapper.innerHTML = "";
+    getAllComments()
+        .then((commentsArray) => {
+            // for(id in commentsArray) {
+            //     comment = commentsArray[id];
+            //     let commentBox = createCommentBox(comment);
+            //     wrapper.append(commentBox);
+            // }
+            const ids = Object.keys(commentsArray);
+            ids.forEach((id) => {
+                comment = commentsArray[id];
+                let commentBox = createCommentBox(comment);
+                wrapper.append(commentBox);
+            });
+        })
+        .catch((reason) => console.log("Message:" + reason.message));
+  };
+  
+  printAllComments();
+
+
+
+//   let response = await fetch(
+//     "https://javascript27g-default-rtdb.firebaseio.com/comments/.json",
+//   );
+//   let data = await response.json();
+//   return data;
+
+
 
 //   const printAllComments1 = (commentsArray) => {
 //     let wrapper = document.getElementById("comment-wrapper");
