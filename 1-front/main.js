f11fe6f56362a6a67c340c0b0119d2983d71cc55
Víctor Cosme <victorcosmema@gmail.com>
2023-07-23 10:18:35 -0600

    Add filter & reduce JS Midu exercises

--- 1-front/main.js
+++ 1-front/main.js
@@ -29,3 +29,45 @@ function endWithA2(words) {
     return words.every( word => (word.endsWith("a")));
 }
 console.log(endWithA2(words2));
+
+// sort numbers -> sort() modify original array
+let num = [12, 3, 24, 51, 9, 19, 33, 21];
+let orderedNum = [...num].sort((a, b) => a - b);
+console.log(num);
+console.log(orderedNum);
+
+// Recibes una lista de números. Debes ordenar los números de menor a mayor según su valor absoluto. Esto 
+// quiere decir que los números negativos pierden el signo y se ordenan como si fueran positivos.
+
+let num2 = [12, -13, 24, -5, 9, -11, 39, 21];
+;
+function sortAbsoluteNumbers3(num2) {
+    return ([...num2].sort((a, b) => Math.abs(a) - Math.abs(b)));
+}
+console.log(sortAbsoluteNumbers3(num2));
+
+// Recibes dos parámetros: una lista de palabras words y una palabra word. Primero, busca el índice de la palabra en la lista. 
+// Después, usa ese índice (que será un número) y devuelve todas las palabras de words que sean más
+// largas (length) que el número del índice.
+
+// Ten en cuenta que la palabra word siempre existirá en el array, por lo que no es necesario comprobar si existe o no.
+
+let wordsList = ["Dev","Koder", "HTML", "CSS", "JS", "Node JS", ]
+
+function searchWords(words) {
+    let result = [];
+    words.forEach((word, index) => {
+        word.length > index ? result.push(word) : null;
+    })
+    return result;
+  }
+console.log(searchWords(wordsList));
+
+function buscarPalabras(words) {
+    return words.reduce((accum, curr, index) => {
+        index < curr.length ? accum.push(curr) : null;
+        return accum;
+    }, [])
+}
+console.log(buscarPalabras(wordsList));
+
