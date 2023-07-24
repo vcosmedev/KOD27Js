c2efd4003804d205cc6a8b65a6560d502d8ebbca
Víctor Cosme <victorcosmema@gmail.com>
2023-07-24 16:08:59 -0600

    Methods arrays methods III

---
+++ 1-front/3-javascript/1.8-arrays-methods-three/methods.js
@@ -0,0 +1,43 @@
+// .map() -> transforma y retorna un arry
+/* 
+Creates a new array from calling a function for every array element.
+Does not execute the function for empty elements.
+Does not change the original array. 
+
+Syntax: array.map(function(currentValue, index, arr), thisValue)
+
+*/
+
+// .filter() -> filtra y retorna un array
+/* 
+- Creates a new array filled with elements that pass a test provided by a function.
+- Does not execute the function for empty elements.
+- Does not change the original array.
+
+Syntax: array.filter(function(currentValue, index, arr), thisValue)
+
+*/
+
+// .forEach() -> no retorna nada -> undefined
+/* 
+Calls a function for each element in an array.
+Is not executed for empty elements. 
+
+Syntax
+array.forEach(function(currentValue, index, arr), thisValue)
+
+*/
+
+// .reduce() -> returns the accumulated result from the last call of the callback function
+/* 
+Executes a reducer function for array element.
+Returns a single value: the function's accumulated result.
+Does not execute the function for empty array elements.
+Does not change the original array. 
+
+Syntax
+array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+
+*/
+
+// .pop() -> 
