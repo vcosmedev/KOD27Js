38713168d5c5c207b839e12af60a04f5d66b00a6
Víctor Cosme <victorcosmema@gmail.com>
2023-08-05 08:27:15 -0600

    Add 3rdh exercise, 2nd BackEnd session

---
+++ 2-back/1.2-second-session/exercise2.js
@@ -0,0 +1,50 @@
+// EXERCISE 2
+// Regresar si el número ingresado es par o impar
+
+const num = process.argv[2];
+const evenOrOdd = () => {
+    if (isNaN(num)) {
+        console.error("You must insert a number");
+        process.exit(1);
+    } else if (num % 2 === 0) {
+        console.log("It's an even number");
+        process.exit(0);
+    } else {
+        console.log("It's an odd number");
+        process.exit(0);
+    }
+}
+ evenOrOdd();
+
+ const evenOrOdd2 = () => {
+    if (isNaN(num)) {
+        console.error("You must insert a number");
+        process.exit(1);
+    } else {
+        if (num % 2 === 0) {
+            console.log("It's an even number");
+        } else {
+            console.log("It's an odd number")
+        }
+        process.exit(0);
+    }
+ }
+ evenOrOdd2();
+
+ const arg = process.argv[2];
+ const number = parseInt(arg); // extract a number from a string -> returned value: an actual number
+
+ if (isNaN(number)) {
+    console.error("Please, you must insert a number");
+    process.exit(1);
+ }
+
+ const isEven = number % 2 === 0;
+ const message = isEven ? "It's an even number" : "It's an odd number";
+ console.log(message);
+ process.exit(0);
+
+ // Console:
+ // node exercise2.js fadaf && echo 'All good :)' -> Please, you must insert a number
+ // node exercise2.js 19 && echo 'All good :)' -> It's an odd number | All good :)
+ // node exercise2.js 12 && echo 'All good :)' -> It's an even number | All good :)
--- 2-back/1.2-second-session/exercises.js
+++ 2-back/1.2-second-session/exercises.js
@@ -1,32 +1,3 @@
-// EXERCISE 2
-// Regresar si el número ingresado es par o impar
-
-// console.log(process.argv[2] % 2 === 0 ? "It's an even number" : process.argv[2] % 2 > 0  ? "It's an odd number" : "It's not a number");
-// process.exit(0);
-// process.exit(1);
-
-// Arrow function with an 'if'
-
-// const num = process.argv[2];
-// const evenOrOdd = () => {
-//     if (isNaN(num)) {
-//         console.error("You must insert a number");
-//         process.exit(1);
-//     } else if (num % 2 === 0) {
-//         console.log("It's an even number");
-//         process.exit(0);
-//     } else {
-//         console.log("It's an odd number");
-//         process.exit(0);
-//     }
-// }
-//  evenOrOdd();
-
- // Add Charle's examples here
-
- /* code here */
-// Ejemplos process + mensaje, echo con operador &&
-
 // EXERCISE 3
 // Crea programa que imprima los número del 1 al 100 y en cada número divisble entre 3
 // que imprima 'Fizz' junto al número, en cada número divisible entre 5 que imprima
@@ -47,7 +18,7 @@
 // process.exit(0);
 
 
-// EXERCISE 3
+// EXERCISE 4
 // Crea programa que imprima los número del 1 al número dado por el usuario y en cada número divisible 
 // entre 3 que imprima 'Fizz' junto al número, en cada número divisible entre 5 que imprima
 // 'Buzz' junto al número y FizzBuzz si es divisible entre ambos
