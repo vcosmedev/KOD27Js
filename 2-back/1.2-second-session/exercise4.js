456575229bb92cea507ee9830b4387d894ec0142
Víctor Cosme <victorcosmema@gmail.com>
2023-08-05 18:17:24 -0600

    Add 4th exercise, 2nd BackEnd session

--- 2-back/1.2-second-session/exercise4.js
+++ 2-back/1.2-second-session/exercise4.js
@@ -1,23 +1,3 @@
-// EXERCISE 3
-// Crea programa que imprima los número del 1 al 100 y en cada número divisble entre 3
-// que imprima 'Fizz' junto al número, en cada número divisible entre 5 que imprima
-// 'Buzz' junto al número y FizzBuzz si es divisible entre ambos
-
-// const fizzBuzz = () => {
-//     for (let i=1; i<=100; i++) {
-//         if (i % 3 === 0 && i % 5 === 0) {
-//             console.log('FizzBuzz', i);
-//         } else if (i % 3 === 0) {
-//             console.log('Fizz', i);
-//         } else if (i % 5 === 0) {
-//             console.log('Buzz', i);
-//         }
-//     }
-// }
-// fizzBuzz();
-// process.exit(0);
-
-
 // EXERCISE 4
 // Crea programa que imprima los número del 1 al número dado por el usuario y en cada número divisible 
 // entre 3 que imprima 'Fizz' junto al número, en cada número divisible entre 5 que imprima
