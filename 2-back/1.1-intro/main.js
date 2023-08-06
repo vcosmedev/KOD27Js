f869b75c33e9a11166a46d17f74e41d863bddf84
Víctor Cosme <victorcosmema@gmail.com>
2023-08-06 17:24:56 -0600

    Udpate 1st BackEnd session

---
+++ 2-back/1.1-intro/main.js
@@ -0,0 +1,23 @@
+/* 
+ Instalar NODE JS como entorno de ejecución
+->  Node Version Manager | NVM -> Manejador de versiones
+1. nvm repo https://github.com/nvm-sh/nvm
+2. copiar comando curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+3. source ~/.zshrc | source ~/.bash
+4. nvm --version
+5. echo $SHELL -> conocer qué Shell se tiene -> Imprime la variable $
+6. cat /etc/shells -> conocer las shells que se tienen disponibles 
+7. chsh -s </shell/route> -> cambiar de shell
+
+-> Reinstalar Node JS
+1. nvm install --lts
+2. node --version / node -v
+3. nvm list -> consulta Node version por default
+4. nvm alias default <versionNumber> -> moverse entre versiones
+
+-> Node Package Manager | NPM
+1. npm --version
+
+*/
+
+// https://github.com/ohmyzsh/ohmyzsh
