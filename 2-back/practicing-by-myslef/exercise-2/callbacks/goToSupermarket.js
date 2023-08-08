0a02c074f22cefd153a9f3391f68dd07c2aa8495
Víctor Cosme <victorcosmema@gmail.com>
2023-08-08 17:06:27 -0600

    Update: asynchronous events and callback hell

--- 2-back/practicing-by-myslef/exercise-2/callbacks/goToSupermarket.js
+++ 2-back/practicing-by-myslef/exercise-2/callbacks/goToSupermarket.js
@@ -6,12 +6,30 @@
 
 callback */
 
+// Manejar eventos asíncronos con callbacks
+
 const goToSupermarket = (callback) => {
   console.log('Yendo al supermercado...');
   setTimeout(() => {
-    // callback(null, 'Hola, ya llegué al supermercado ;D');
-    callback('Hubo una manifestación D:', null);
-  }, 5000);
+    callback(null, 'Ya llegué al supermercado ;D');
+    // callback('Hubo una manifestación D:', null);
+  }, 1000);
+  // Ya llegó al supermercado
+};
+
+const pay = (callback) => {
+  console.log('Estoy haciendo fila para pagar...');
+  setTimeout(() => {
+    // callback(null, 'Finally, ya pagué la despensa :)))');
+    callback('Se me olvidó el monedero :O', null);
+  }, 3000);
+};
+
+const home = (callback) => {
+  console.log('Yendo a casa...');
+  setTimeout(() => {
+    callback(null, 'Ya estoy en casa 🏡');
+  }, 4000);
 };
 
 goToSupermarket((error, message) => {
@@ -22,6 +40,23 @@ goToSupermarket((error, message) => {
   }
   console.log(message);
   // if...else -> Es lo mismo, código más limpio
+
+  pay((error, message) => {
+    if (error) {
+      console.log('Error:', error);
+      return error;
+    }
+    console.log(message);
+
+    home((error, message) => {
+      if (error) {
+        console.log('Hubo un error', error);
+      }
+      console.log(message);
+    });
+  });
 });
 
-// el bloque de código anterior evalúa un truthy y falsy
+// Los bloques de código anterior '(error, message)' evalúa un truthy / falsy
+
+// Anidar asincronismo en JS -> callback hell 👹
