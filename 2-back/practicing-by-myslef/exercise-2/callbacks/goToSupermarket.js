594213dec2ab2de2991b1fa812f792ef578dc240
Víctor Cosme <victorcosmema@gmail.com>
2023-08-08 17:31:59 -0600

    Update: asynchronous events and callback hell

--- 2-back/practicing-by-myslef/exercise-2/callbacks/goToSupermarket.js
+++ 2-back/practicing-by-myslef/exercise-2/callbacks/goToSupermarket.js
@@ -9,52 +9,52 @@ callback */
 // Manejar eventos asíncronos con callbacks
 
 const goToSupermarket = (callback) => {
-  console.log('Yendo al supermercado...');
-  setTimeout(() => {
-    callback(null, 'Ya llegué al supermercado ;D');
-    // callback('Hubo una manifestación D:', null);
-  }, 1000);
-  // Ya llegó al supermercado
+    console.log('Yendo al supermercado...');
+    setTimeout(() => {
+        callback(null, 'Ya llegué al supermercado ;D');
+        // callback('Hubo una manifestación D:', null);
+    }, 1000);
+    // Ya llegó al supermercado
 };
 
 const pay = (callback) => {
-  console.log('Estoy haciendo fila para pagar...');
-  setTimeout(() => {
-    // callback(null, 'Finally, ya pagué la despensa :)))');
-    callback('Se me olvidó el monedero :O', null);
-  }, 3000);
+    console.log('Estoy haciendo fila para pagar...');
+    setTimeout(() => {
+        // callback(null, 'Finally, ya pagué la despensa :)))');
+        callback('Se me olvidó el monedero :O', null);
+    }, 3000);
 };
 
 const home = (callback) => {
-  console.log('Yendo a casa...');
-  setTimeout(() => {
-    callback(null, 'Ya estoy en casa 🏡');
-  }, 4000);
+    console.log('Yendo a casa...');
+    setTimeout(() => {
+        callback(null, 'Ya estoy en casa 🏡');
+    }, 4000);
 };
 
 goToSupermarket((error, message) => {
-  if (error) {
-    console.log('Ha ocurrido algo:', error);
-    return error; // Retorna 'undefined'
-    // Detener el error, termina la ejecución aquí -> Salidas tempranas o salidas condicionales
-  }
-  console.log(message);
-  // if...else -> Es lo mismo, código más limpio
-
-  pay((error, message) => {
     if (error) {
-      console.log('Error:', error);
-      return error;
+        console.log('Ha ocurrido algo:', error);
+        return error; // Retorna 'undefined'
+        // Detener el error, termina la ejecución aquí -> Salidas tempranas o salidas condicionales
     }
     console.log(message);
+    // if...else -> Es lo mismo, código más limpio
 
-    home((error, message) => {
-      if (error) {
-        console.log('Hubo un error', error);
-      }
-      console.log(message);
+    pay((error, message) => {
+        if (error) {
+            console.log('Error:', error);
+            return error;
+        }
+        console.log(message);
+
+        home((error, message) => {
+            if (error) {
+                console.log('Hubo un error', error);
+            }
+            console.log(message);
+        });
     });
-  });
 });
 
 // Los bloques de código anterior '(error, message)' evalúa un truthy / falsy
