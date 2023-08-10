70b9b2d3f8d0b4e2885abddf6aef6e7315616fba
Víctor Cosme <victorcosmema@gmail.com>
2023-08-10 00:19:22 -0600

    Add copyFileSync advanced

--- 2-back/1.5-file-system/thirdNodeProject/copy.js
+++ 2-back/1.5-file-system/thirdNodeProject/copy.js
@@ -2,3 +2,8 @@ const fs = require('fs');
 
 const copy = fs.copyFileSync('hello.txt', 'hello-copy.txt');
 copy ? console.log('Archivo copiado correctamente') : null;
+
+// Advanced
+const { copyFileSync } = require('node:fs');
+copyFileSync('hello.txt', 'hello-copy-adv.txt');
+copyFileSync ? console.log('Archivo copiado correctamente adv :)))') : null;
