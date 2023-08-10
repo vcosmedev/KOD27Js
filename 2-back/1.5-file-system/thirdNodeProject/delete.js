5aa9ae7e1e81511203d1cbf7e5f66ed6e68b66c9
Víctor Cosme <victorcosmema@gmail.com>
2023-08-10 00:15:08 -0600

    Add unlinkSync advanced

--- 2-back/1.5-file-system/thirdNodeProject/delete.js
+++ 2-back/1.5-file-system/thirdNodeProject/delete.js
@@ -2,3 +2,7 @@ const fs = require('fs');
 
 const deleted = fs.unlinkSync('hello-delete.txt');
 deleted ? console.log('Archivo eliminado con éxito') : null;
+
+// Advanced
+// const { unlinkSync } = require('node:fs');
+// unlinkSync('hello-delete.text');
