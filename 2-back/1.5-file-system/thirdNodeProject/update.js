c336a4a940e845b00e225123b0aa8e15f22f4584
Víctor Cosme <victorcosmema@gmail.com>
2023-08-10 00:26:09 -0600

    Add appendFileSync advanced

--- 2-back/1.5-file-system/thirdNodeProject/update.js
+++ 2-back/1.5-file-system/thirdNodeProject/update.js
@@ -2,7 +2,12 @@ const fs = require('fs');
 
 const update = fs.appendFileSync(
     'hello.txt',
-    'Data to append from udpate.js',
+    '\n Data to append from udpate.js\n',
     'utf8'
 );
 update ? console.log('Archivo modificado con éxito') : null;
+
+// Advanced
+const { appendFileSync } = require('node:fs');
+appendFileSync('hello.txt', 'Data updated using appendFileSync', 'utf-8');
+appendFileSync ? console.log('Data successfully added') : null;
