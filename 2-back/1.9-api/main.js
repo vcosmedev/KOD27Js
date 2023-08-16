c5cd14375722d20da575c9fa5a39f76cc26021db
Víctor Cosme <victorcosmema@gmail.com>
2023-08-15 20:26:52 -0600

    Add api node project structure

---
+++ 2-back/1.9-api/main.js
@@ -0,0 +1,38 @@
+/* 
+
+API  Application Programming Interface
+
+Interfaz que es utilizada por desarrolladores para conectar un sistema A con un sistema B.
+Conjunto de funciones y procedimientos que cumplen una o muchas funciones con el fin de ser utilizdas por otro software.
+
+Interfaz: algo que permite comunicar un ente A con un ente B, una cosa con otra (Terminal, por ejemplo)
+
+Ejemplos de interfaces:
+- FileSystem API
+    * writeFileSync()
+    * mkdir()
+- DOM API
+    * getElementById()
+    * on('event')
+    * etc...
+- Bootstrap
+
+- Pokeapi
+- Spotify API
+- Swapi
+- Facebook API
+- Rickandmortyapi
+
+¿QUÉ ES REST Y RESTFUL?
+REST es una ARQUITECTURA de software. API estructurada ocupando tecnologías específicas.
+- Protocolo cliente servidor sin estado (stateless) (http)
+- Conjunto de operaciones bien definidas (verbos http)
+- Sintaxis universal (/recurso/identificador)
+- Uso de hipermedia (hipermedios: html, xml, json)
+
+Se denomina RESTful a un servicio que implementa la arquitectura REST
+
+EXPRESS ROUTING
+Router: conjunto de rutas/operaciones que están ordenadas de forma semántica
+
+*/
