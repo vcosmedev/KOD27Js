159ee32948c66c7b2e62605d7bff5fb382f8e437
Víctor Cosme <victorcosmema@gmail.com>
2023-08-15 19:21:47 -0600

    Add README practice 5 and git ignore file

--- 2-back/1.3-third-session/myFirstNodeProject/index.js
+++ 2-back/1.3-third-session/myFirstNodeProject/index.js
@@ -8,21 +8,6 @@
 // Para utilizar cualquier paquete y hacer uso de él, LEER DOCUMENTACIÓN
 // https://www.npmjs.com/package/prompt-sync
 
-/* PRÁCTICA 5
-
-Crear un programa que le pida a un usuario ingresar una lista de nombres, 
-el número de nombres permitido es indeterminado.
-
-Una vez el usuario no desee ingresar más nombres, el programa le da al 
-usuario la siguiente información:
-
-1- Cuántos nombres se ingresaron
-2- Si existe al menos un valor repetido o no
-- Cuál es el nombre más largo de todos los ingresados
-- Cuál es el nombre más corto de todos los ingresados
-
-*/
-
 // const namesList = [];
 
 const prompt = require('prompt-sync')();
