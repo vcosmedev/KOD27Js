d7eccdc4193575e66d3ab47c5b520052754759fb
Víctor Cosme <victorcosmema@gmail.com>
2023-08-18 00:09:41 -0600

    Add Mentors router and useCase

--- 2-back/1.11-clean-arquitecture/src/routes/mentors.router.js
+++ 2-back/1.11-clean-arquitecture/src/routes/mentors.router.js
@@ -1,21 +1,33 @@
-// const express = require('express');
-// const mentors = require('../useCases/mentors.useCase');
+const express = require('express');
+const mentors = require('../useCases/mentors.useCase');
 
-// const router = express.Router();
+const router = express.Router();
 
-// // List Mentors
-// router.get('/', (req, res) => {
-//     const allMentors = mentors.getAll();
-//     res.json({
-//         message: 'GET mentors with clean arquitecture',
-//         mentors: allMentors,
-//     });
-// });
+// List Mentors
+router.get('/', (req, res) => {
+    const allMentors = mentors.getAll();
+    res.json({
+        message: 'GET mentors with clean arquitecture',
+        mentors: allMentors,
+    });
+});
 
-// // Create Mentor
-// // Endpoint, controlador
-// router.post('/', (req, res) => {
-//     const { name, email, languages } = req.body;
-// });
+// Create Mentor
+// Endpoint, controlador
+router.post('/', (req, res) => {
+    const { name, email, languages } = req.body;
+    mentors.create(name, email, languages);
+    res.json({ message: 'Mentor added' });
+});
 
-// module.exports = router;
+// Update Koder
+// koders.update()
+
+// Delete Koder
+router.delete('/:name', (req, res) => {
+    const { name } = req.params;
+    mentors.remove(name);
+    res.json({ message: 'Mentor deleted' });
+});
+
+module.exports = router;
--- 2-back/1.11-clean-arquitecture/src/useCases/mentors.useCase.js
+++ 2-back/1.11-clean-arquitecture/src/useCases/mentors.useCase.js
@@ -1,21 +1,34 @@
-// const db = require('../lib/db');
+const db = require('../lib/db');
 
-// function create(name, email, languages) {
-//     const newMentor = { name, email, languages };
-//     // Leemos info db
-//     const data = db.getData();
-//     // Agregamos a la lista
-//     data.mentors.push(newMentor);
-//     // Leemos la lista actualizada
-//     db.updateData(data);
-// }
+function create(name, email, languages) {
+    const newMentor = { name, email, languages };
+    // Leemos info db
+    const data = db.getData();
+    // Agregamos a la lista
+    data.mentors.push(newMentor);
+    // Leemos la lista actualizada
+    db.updateData(data);
+}
 
-// function remove() {}
-// function update() {}
-// function getAll() {
-//     const data = db.getData();
-//     // Caso de uso de Mentors, obtenemos todos los Mentors
-//     return data.koders;
-// }
+function remove(name) {
+    // Leemos info db
+    const data = db.getData();
+    // Buscamos mentor en la lista
+    const newMentorsList = data.mentors.filter(
+        (mentor) => mentor.name !== name
+    );
+    // Sustituir arreglo antiguo por el nuevo
+    data.mentors = newMentorsList;
+    // Actualizar data
+    db.updateData(data);
+}
 
-// module.exports = { create, remove, update, getAll };
+function update() {}
+
+function getAll() {
+    const data = db.getData();
+    // Caso de uso de Mentors, obtenemos todos los Mentors
+    return data.mentors;
+}
+
+module.exports = { create, remove, update, getAll };
