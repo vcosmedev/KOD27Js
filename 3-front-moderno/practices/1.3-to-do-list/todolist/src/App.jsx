0fa6072ff05e0f950837825e179c4b52d68eac0f
Víctor Cosme <victorcosmema@gmail.com>
2023-09-02 15:28:12 -0600

    Add tasks to ToDo List

--- 3-front-moderno/practices/1.3-to-do-list/todolist/src/App.jsx
+++ 3-front-moderno/practices/1.3-to-do-list/todolist/src/App.jsx
@@ -1,35 +1,128 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [task, setTask] = useState(null);
+    const [priority, setPriority] = useState('LOW');
+    const [taskList, setTaskList] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const taskHandler = (event) => {
+        const value = event.target.value;
+        setTask(value);
+    };
+
+    const priorityHandler = (event) => {
+        const value = event.target.value;
+        setPriority(value);
+    };
+
+    // Add new task
+    const addNewTaskHandler = () => {
+        const newTask = [task];
+        setTaskList([...taskList, newTask]);
+    };
+
+    return (
+        <>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-12 col-md-6'>
+                        <h1>ToDo List</h1>
+                        <ul className='list-group'>
+                            {taskList.map((newTask) => {
+                                return (
+                                    <li className='list-group-item d-flex justify-content-center'>
+                                        {newTask}
+                                        <input
+                                            className='form-check-input ms-2'
+                                            type='checkbox'
+                                            value=''
+                                            id='flexCheck'
+                                            // onChange
+                                        />
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                    <div className='col-12 col-md-6'>
+                        <h3 className='ms-4'>New ToDo Task</h3>
+                        <div className='form-check'>
+                            <input
+                                className='form-control'
+                                type='text'
+                                placeholder='New task input'
+                                onChange={taskHandler}
+                            />
+                        </div>
+                        <h4 className='ms-4 mt-3'>Priority</h4>
+                        <div className='form-check'>
+                            <input
+                                className='form-check-input'
+                                type='radio'
+                                name='priority'
+                                id='lowPriority'
+                                // Watch out here ⤵
+                                value='LOW'
+                                checked={priority === 'LOW'}
+                                onChange={priorityHandler}
+                            />
+                            <label
+                                className='form-check-label'
+                                htmlFor='flexRadioDefault2'
+                            >
+                                Low
+                            </label>
+                        </div>
+                        <div className='form-check'>
+                            <input
+                                className='form-check-input'
+                                type='radio'
+                                name='priority'
+                                id='mediumPriority'
+                                // Watch out here ⤵
+                                value='MEDIUM'
+                                checked={priority === 'MEDIUM'}
+                                onChange={priorityHandler}
+                            />
+                            <label
+                                className='form-check-label'
+                                htmlFor='flexRadioDefault1'
+                            >
+                                Medium
+                            </label>
+                        </div>
+                        <div className='form-check'>
+                            <input
+                                className='form-check-input'
+                                type='radio'
+                                name='priority'
+                                id='highPriority'
+                                // Watch out here ⤵
+                                value='HIGH'
+                                checked={priority === 'HIGH'}
+                                onChange={priorityHandler}
+                            />
+                            <label
+                                className='form-check-label'
+                                htmlFor='flexRadioDefault1'
+                            >
+                                High
+                            </label>
+                        </div>
+                        <button
+                            type='button'
+                            className='btn btn-primary mt-3 ms-4'
+                            onClick={addNewTaskHandler}
+                        >
+                            Save
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
