1131de450bc74ec3221cba37263265b423a5a1af
Víctor Cosme <victorcosmema@gmail.com>
2023-09-07 03:44:01 -0600

    Add GET and POST methods

--- 3-front-moderno/1.6-fetch/fetch/src/App.jsx
+++ 3-front-moderno/1.6-fetch/fetch/src/App.jsx
@@ -1,87 +1,102 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-    const [entry, setEntries] = useState({});
+    const [entries, setEntries] = useState({});
+    const [update, setUpdate] = useState(false);
+    const [entryData, setEntryData] = useState({});
 
-    const inputHandler = () => {
-        setEntries({ ...entry, [event.target.name]: event.target.value });
+    useEffect(() => {
+        const getEntries = async () => {
+            const response = await fetch(
+                'https://javascript27kod-default-rtdb.firebaseio.com/playlist.json'
+            );
+            const data = await response.json();
+            setEntries(data);
+        };
+        getEntries();
+    }, [update]);
+
+    // Update Handler
+    const updateHandler = () => setUpdate(!update);
+
+    //Input Handler
+    const inputHandler = (event) => {
+        setEntryData({ ...entryData, [event.target.name]: event.target.value });
     };
 
-    // useEffect(() => {
-    //     const getEntries = () => {
-    //         const response = fetch(
-    //             'https://javascript27g-default-rtdb.firebaseio.com/playlist'
-    //         );
-    //         const data = response.json();
-    //         setEntries(data);
-    //     };
-    //     getEntries();
-    // }, []);
+    // Save entry
+    const saveEntry = async () => {
+        const response = await fetch(
+            'https://javascript27kod-default-rtdb.firebaseio.com/playlist.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(entryData),
+            }
+        );
+        const data = await response.json();
+        setUpdate(!update);
+        console.log(data);
+    };
+
     return (
         <>
             <div className='container'>
                 <div className='row'>
                     <div className='col-12 col-md-6'>
-                        <h3>Registra una canción</h3>
+                        <h3>Song registration</h3>
+                        <button
+                            type='submit'
+                            className='btn btn-primary'
+                            onClick={saveEntry}
+                        >
+                            Save
+                        </button>
                         <form>
-                            <div class='mb-3'>
-                                <label
-                                    for='exampleInputEmail1'
-                                    class='form-label'
-                                >
-                                    Song Title
-                                </label>
-                                <input
-                                    type='text'
-                                    class='form-control'
-                                    id='songName'
-                                    aria-describedby='songName'
-                                    name='name'
-                                    onChange={inputHandler}
-                                />
-                            </div>
-                            <div class='mb-3'>
-                                <label
-                                    for='exampleInputEmail1'
-                                    class='form-label'
-                                >
-                                    Artist
-                                </label>
-                                <input
-                                    type='text'
-                                    class='form-control'
-                                    id='songName'
-                                    aria-describedby='songName'
-                                    name='artist'
-                                    onChange={inputHandler}
-                                />
-                            </div>
+                            <label
+                                htmlFor='exampleInputEmail1'
+                                className='form-label'
+                            ></label>
+                            <input
+                                type='text'
+                                className='form-control'
+                                id='songName'
+                                aria-describedby='songName'
+                                placeholder='Song Title'
+                                name='name'
+                                onChange={inputHandler}
+                            />
+                            <label
+                                htmlFor='exampleInputEmail1'
+                                className='form-label'
+                            ></label>
+                            <input
+                                type='text'
+                                className='form-control'
+                                id='songName'
+                                aria-describedby='songName'
+                                placeholder='Artist'
+                                name='artist'
+                                onChange={inputHandler}
+                            />
+
                             <button
-                                type='submit'
-                                class='btn btn-primary'
-                                // onClick={test}
+                                className='btn btn-primary mt-3'
+                                onClick={updateHandler}
                             >
-                                Save
+                                Update List
                             </button>
                         </form>
                     </div>
-                    <div className='col-12 col-md-6'>
-                        <h3>Lista de canciones</h3>
-                        <ul class='list-group'>
-                            {/* koders.map((koder) => {
-                            const {name, lastname, generation} = koder;
-                            return (
-                              <li className='list-group-item'>{`${name} ${lastname} ${generation}`}
-                              </li>
-                            )
-                           }) */}
-                            <li class='list-group-item'>An item</li>
-                            <li class='list-group-item'>A second item</li>
-                            <li class='list-group-item'>A third item</li>
-                            <li class='list-group-item'>A fourth item</li>
-                            <li class='list-group-item'>And a fifth one</li>
+                    <div className='col-12 col-md-6 mt-3'>
+                        <h3>Songs List 🎵</h3>
+                        <ul className='list-group'>
+                            {Object.keys(entries).map((key) => (
+                                <li key={key} className='list-group-item'>
+                                    {entries[key].name} - {entries[key].artist}
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 </div>
