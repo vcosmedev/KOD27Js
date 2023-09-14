7cd1ee42360789e6fc734bcdd59d7d4097e35b93
Víctor Cosme <victorcosmema@gmail.com>
2023-09-13 21:30:46 -0600

    Add userData handler

--- 3-front-moderno/practices/1.5-users-list/users-list/src/App.jsx
+++ 3-front-moderno/practices/1.5-users-list/users-list/src/App.jsx
@@ -1,21 +1,25 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [user, setUser] = useState(null);
+    const [userData, setUserData] = useState({});
+
+    // userData Handler
+    const inputHanlder = (event) => {
+        setUserData({ ...user, [event.target.name]: event.target.value });
+    };
 
     return (
         <>
             <div className='container'>
                 <div className='row'>
-                    <div className='col-12 col-md-3 mt-3'>
+                    <div className='col-12 col-md-4 pb-3 mt-3'>
                         <h3 className='m-0'>Login form</h3>
                         <form>
                             <div className=''>
                                 <label
-                                    for='exampleInputEmail1'
+                                    htmlFor='exampleInputEmail1'
                                     className='form-label'
                                 ></label>
                                 <input
@@ -24,11 +28,13 @@ function App() {
                                     id='name'
                                     aria-describedby='emailHelp'
                                     placeholder='Name'
+                                    name='name'
+                                    onChange={inputHanlder}
                                 />
                             </div>
                             <div className='mb-3'>
                                 <label
-                                    for='exampleInputPassword1'
+                                    htmlFor='exampleInputPassword1'
                                     className='form-label'
                                 ></label>
                                 <input
@@ -36,15 +42,22 @@ function App() {
                                     className='form-control'
                                     id='exampleInputPassword1'
                                     placeholder='Email'
+                                    name='email'
+                                    onChange={inputHanlder}
                                 />
                             </div>
 
-                            <button type='submit' className='btn btn-primary'>
+                            <button
+                                type='submit'
+                                className='btn btn-primary mb-3 '
+                            >
                                 Save
                             </button>
                         </form>
                     </div>
-                    <h3>Users list</h3>
+                    <div className='col-12 col-md-4 pb-3 mt-3'>
+                        <h3>Users list</h3>
+                    </div>
                 </div>
             </div>
         </>
