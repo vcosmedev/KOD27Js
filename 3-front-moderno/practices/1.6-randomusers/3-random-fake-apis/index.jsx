88fc7b166093f586bab00aee593adec6895f79a6
Víctor Cosme <victorcosmema@gmail.com>
2023-09-17 01:51:25 -0600

    Add randomuser and fakestore project

---
+++ 3-front-moderno/practices/1.6-randomusers/3-random-fake-apis/index.jsx
@@ -0,0 +1,5 @@
+// Replicar ejercicio User-Products alimentando, en este caso, la información
+// con los datos que extraemos de las APIs randomuser y fakestore y
+// completar las vistas que faltaban (home, users, products)
+
+// branch -> random-fake-api
